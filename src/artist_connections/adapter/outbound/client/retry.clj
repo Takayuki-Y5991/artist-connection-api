b569e8debd7275f0423dac8f6c86869534e32497
konkon <konkon@debian.konkon>
2025-02-26 22:45:54 +0900

    :snake: added ports

--- src/artist_connections/adapter/outbound/client/retry.clj
+++ src/artist_connections/adapter/outbound/client/retry.clj
@@ -12,11 +12,11 @@
   "指定された回数まで操作をリトライします。
 
      options:
-     - :max-attempts - default 3
-     - :backoff-ms - default 1000
-     - :backoff-factor - default 2
-     - :jitter - default 0.1
-     - :retryable? - true
+   - :max-attempts - 最大試行回数（デフォルト: 3）
+   - :backoff-ms - バックオフ時間（ミリ秒）（デフォルト: 1000）
+   - :backoff-factor - 指数バックオフの係数（デフォルト: 2）
+   - :jitter - バックオフ時間のジッター（デフォルト: 0.1）
+   - :retryable? - リトライ可能なエラーを判断する関数（デフォルト: 常にtrue）
 
      Example:
      (retry #(http-call \"https://example.com\")
@@ -33,7 +33,11 @@
       (let [result (apply f args)]
         (cond
           (success? result) result
+          ;;  return failure, If the maximum number of attempts is reached.
           (>= attempts max-attempts) result
+          ;; return failure, If the error is not retryable
+          (not (retryable? (:error result))) result
+
           :else (do
                   (let [jitter-amount (* current-backoff jitter (- (rand) 0.5))
                         sleep-ms (+ current-backoff jitter-amount)]
