20e413a3ad0d92de2a12876eb402f0af82d0b6ad
konkon <konkon@debian.konkon>
2025-03-01 10:49:48 +0900

    :seal: lazy, async process for middlware and response

--- src/artist_connections/adapter/inbound/middleware.clj
+++ src/artist_connections/adapter/inbound/middleware.clj
@@ -1,9 +1,22 @@
 (ns artist-connections.adapter.inbound.middleware
   (:require
-   [artist-connections.macros.railway :refer [failure? success?]]
+   [artist-connections.macros.railway :refer [failure? success? lazy? ->value ->error]]
+   [artist-connections.macros.railway-async :as a :refer [channel?]]
    [cheshire.core :as json]
+   [clojure.core.async :refer [<! go <!]]
    [ring.util.response :as response]))
 
+;; Helper to synchronously extract values from channel
+(defn <!! [ch]
+  (let [p (promise)]
+    (go
+      (let [v (<! ch)]
+        (deliver p v)))
+    (deref p 5000 {:status 500
+                   :body {:error "Timeout while processing request"}
+                   :headers {"Content-Type" "application/json"}})))
+
+;; JSON response middleware (unchanged)
 (defn wrap-json-response [handler]
   (fn [request]
     (let [response (handler request)]
@@ -28,6 +41,7 @@
               (update :body #(if (string? %) % (json/generate-string %)))
               (response/content-type "application/json")))))))
 
+;; Error handling middleware (unchanged)
 (defn wrap-error-handling [handler]
   (fn [request]
     (try
@@ -38,76 +52,97 @@
             (response/status 500)
             (response/content-type "application/json"))))))
 
+;; Process railway result into HTTP response
+(defn process-railway-result [result]
+  (println "PROCESSING RAILWAY RESULT - TYPE:" (type result))
+
+  (cond
+    ;; Success case
+    (success? result)
+    (let [value (if (lazy? result) (->value result) (:value result))]
+      (println "PROCESSING Success TYPE. VALUE TYPE:" (type value))
+      (cond
+        ;; SuccessResponse case
+        (and (map? value) (:body value))
+        (do
+          (println "PROCESSING SuccessResponse")
+          (-> (:body value)
+              (response/response)
+              (response/status 200)
+              (response/content-type "application/json")))
+
+        ;; Other cases
+        :else
+        (do
+          (println "PROCESSING Other Value Type")
+          (-> value
+              (response/response)
+              (response/status 200)
+              (response/content-type "application/json")))))
+
+    ;; Failure case
+    (failure? result)
+    (let [error (if (lazy? result) (->error result) (:error result))]
+      (println "PROCESSING Failure Type. ERROR TYPE:" (type error))
+      (cond
+        (and (map? error) (:status error) (:message error))
+        (do
+          (println "PROCESSING ErrorResponse")
+          (-> {:error_code (name (:status error))
+               :message (:message error)}
+              (response/response)
+              (response/status (:status error))
+              (response/content-type "application/json")))
+
+        ;; Other cases
+        :else
+        (do
+          (println "PROCESSING Other Error Type")
+          (-> {:error error}
+              (response/response)
+              (response/status 500)
+              (response/content-type "application/json")))))
+
+    ;; Already a Ring response
+    (and (map? result) (contains? result :status))
+    (do
+      (println "ALREADY A RING RESPONSE")
+      result)
+
+    ;; Fallback case
+    :else
+    (do
+      (println "FALLBACK CASE - TYPE:" (type result))
+      (-> result
+          (response/response)
+          (response/status 200)
+          (response/content-type "application/json")))))
+
+;; Enhanced middleware to handle sync, async, and lazy railway results
 (defn wrap-railway-response [handler]
   (fn [request]
     (println "ENTERING wrap-railway-response middleware")
     (let [result (handler request)]
       (println "IN wrap-railway-response - RESULT TYPE:" (type result))
 
-      (let [response (cond
-                       (success? result)
-                       (let [value (:value result)]
-                         (println "PROCESSING Success TYPE. VALUE TYPE:" (type value))
-                         (cond
-                           ;; SuccessResponse の場合
-                          ;;  (instance? artist_connections.adapter.inbound.response.SuccessResponse value)
-                           (and (map? value) (:body value))
-                           (do
-                             (println "PROCESSING SuccessResponse")
-                             (-> (:body value)
-                                 (response/response)
-                                 (response/status 200)
-                                 (response/content-type "application/json")))
+      (cond
+        ;; Handle async (channel) results
+        (channel? result)
+        (do
+          (println "HANDLING ASYNC RESULT")
+          (<!! (go (process-railway-result (<! result)))))
 
-                           ;; その他の場合
-                           :else
-                           (do
-                             (println "PROCESSING Other Value Type")
-                             (-> value
-                                 (response/response)
-                                 (response/status 200)
-                                 (response/content-type "application/json")))))
+        ;; Handle lazy results
+        (lazy? result)
+        (do
+          (println "HANDLING LAZY RESULT")
+          (process-railway-result result))
 
-                       (failure? result)
-                       (let [error (:error result)]
-                         (println "PROCESSING Failure Type. ERROR TYPE:" (type error))
-                         (cond
-                           (and (map? error) (:status error) (:message error))
-                           (do
-                             (println "PROCESSING ErrorResponse")
-                             (-> {:error_code (name (:status error))
-                                  :message (:message error)}
-                                 (response/response)
-                                 (response/status (:status error))
-                                 (response/content-type "application/json")))
+        ;; Handle synchronous results
+        :else
+        (process-railway-result result)))))
 
-                           ;; その他の場合
-                           :else
-                           (do
-                             (println "PROCESSING Other Error Type")
-                             (-> {:error error}
-                                 (response/response)
-                                 (response/status 500)
-                                 (response/content-type "application/json")))))
-
-                       ;; 既に Ring レスポンスの場合
-                       (and (map? result) (contains? result :status))
-                       (do
-                         (println "ALREADY A RING RESPONSE")
-                         result)
-
-                       ;; その他の場合
-                       :else
-                       (do
-                         (println "FALLBACK CASE - TYPE:" (type result))
-                         (-> result
-                             (response/response)
-                             (response/status 200)
-                             (response/content-type "application/json"))))]
-
-        (println "EXITING wrap-railway-response - RESPONSE TYPE:" (type response))
-        response))))
-
+;; Debug middleware (unchanged)
 (defn debug-response-middleware [handler]
   (fn [request]
     (println "BEFORE HANDLER - REQUEST:" request)
